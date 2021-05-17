0050484ced19ad6744bbf28e4c3f6649aa14aaef
kurnikarz <arturvxx@o2.pl>
2021-05-18 00:16:08 +0200

    wyświetlenie szczegółow zamówienia

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\PsAddress;
 use App\Entity\PsCustomer;
+use App\Entity\PsOrderDetail;
 use App\Entity\PsOrders;
+use App\Entity\PsProduct;
+use App\Entity\PsProductLang;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,21 +20,45 @@ class HomepageController extends AbstractController
     public function index(): Response
     {
         $orders = $this->getDoctrine()->getRepository(PsOrders::class)->findAll();
-        $customers = $this->getDoctrine()->getRepository(PsCustomer::class)->find(3);
         $ordersArray = [];
 
         foreach ($orders as $order) {
+            $price = 0;
+            $productsArray = [];
             $customer = $this->getDoctrine()->getRepository(PsCustomer::class)->find($order->getIdCustomer());
-            array_push($ordersArray, [
-                'Customer' => $customer->getFirstName().' '.$customer->getLastName()
+            $address = $this->getDoctrine()->getRepository(PsAddress::class)->findOneBy([
+                'idCustomer' => $customer->getIdCustomer()
             ]);
+
+            $OrderProducts = $this->getDoctrine()->getRepository(PsOrderDetail::class)->findBy([
+                'idOrder' => $order->getIdOrder()
+            ]);
+
+            foreach ($OrderProducts as $OrderProduct) {
+                $product = $this->getDoctrine()->getRepository(PsProductLang::class)->findOneBy([
+                    'idProduct' => $OrderProduct->getProductId()
+                ]);
+                array_push($productsArray, [
+                    'Name' => $OrderProduct->getProductQuantity().'x '. $product->getName()
+                ]);
+                $price += $OrderProduct->getTotalPriceTaxIncl();
+            }
+
+            array_push($ordersArray, [
+                'Customer' => $customer->getFirstName().' '.$customer->getLastName(),
+                'Address' => $address->getAddress1().' '.$address->getAddress2().' '.$address->getPostcode().' '.$address->getCity(),
+                'Products' => $productsArray,
+                'Price' => $price,
+                'Phone' => $address->getPhone()
+            ]);
+
+            unset($productsArray);
         }
 
         dump($ordersArray);
 
         return $this->render('homepage/index.html.twig', [
-            'orders' => $orders,
-            'customers' => $customers
+            'orders' => $ordersArray,
         ]);
     }
 }
